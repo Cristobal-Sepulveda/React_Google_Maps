ab1104a656739d3c92791f420a4816be4fb2a884
Cristobal Sepulveda <cristobal.sepulveda@agrosat.cl>
2022-04-11 12:10:47 -0400

    marker agregado

--- App.js
+++ App.js
@@ -1,11 +1,36 @@
-import * as React from 'react';
-import MapView from 'react-native-maps';
+import React, {useState, useEffect} from 'react';
+import {Button} from 'react-native'
+import MapView, {Marker} from 'react-native-maps';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
 export default function App() {
+  const [initialLocation, setInitialLocation] = useState({latitude: -33.399455892461916,
+                                                          longitude: -70.5762546898385,
+                                                          latitudeDelta: 0.005,
+                                                          longitudeDelta: 0.005})
+
+  const [initialMarker,setInitialMarker] = useState({latitude: -33.399455892461916, longitude: -70.5762546898385})
+                                                           
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} />
+      <Button title = "googleMaps"/>
+      <MapView style={styles.map} region={initialLocation}>
+        {/* Logic to render a list of markers on the map*/}
+        {/*{this.state.markers.map((marker, index) => (
+          <Marker
+            key={index}
+            coordinate={marker.latlng}
+            title={marker.title}
+            description={marker.description}
+          />
+        ))} */}
+        <Marker
+            key={"Agrosat"}
+            coordinate={initialMarker}
+            title={"Agrosat Internacional"}
+            description={"Agrosat"}
+          />
+      </MapView>
     </View>
   );
 }
